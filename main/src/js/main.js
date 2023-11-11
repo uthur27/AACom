7a8d8d34736bae3372cf3144a2b2ad55baeaafb4
uthur <uthur27@gmail.com>
2023-11-12 00:40:23 +0900

    AAC_Page commit

--- main/src/js/main.js
+++ main/src/js/main.js
@@ -1,3 +1,11 @@
+const login = document.getElementById("login-wrapper");
+const loginS = document.getElementById("login-success-wrapper");
+
 function loginPage() {
   window.location.href = "login.html";
+}
+
+function loginTest() {
+  login.style.display = "none";
+  loginS.style.display = "block";
 }
